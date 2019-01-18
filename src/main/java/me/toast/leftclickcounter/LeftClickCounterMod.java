b95f80933919b7d322e7bcc239ed401db94a5b45
Litl Toast <joshuafhiggins@gmail.com>
2019-01-18 09:56:27 -0500

    Removed Keybinds and RightClickCounterListener

--- src/main/java/me/toast/leftclickcounter/LeftClickCounterMod.java
+++ src/main/java/me/toast/leftclickcounter/LeftClickCounterMod.java
@@ -1,12 +1,7 @@
 package me.toast.leftclickcounter;
 
-import org.lwjgl.input.Keyboard;
-
 import me.toast.leftclickcounter.commands.LeftClickCounterCommand;
-import me.toast.leftclickcounter.keybinds.KeyBindings;
 import me.toast.leftclickcounter.listeners.LeftClickCounterListener;
-import me.toast.leftclickcounter.listeners.RightClickCounterListener;
-import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.common.MinecraftForge;
@@ -23,23 +18,14 @@ public class LeftClickCounterMod
     public static final String MODID = "leftclickcounter";
     public static final String VERSION = "1.1.0";
     
-    //TODO Fix en_US lang file for keybinds.
+	//TODO Fix en_US lang file for keybinds.
 	//TODO Make a config for storing clicks and positions for text.
-    
-    @EventHandler
-    public void preinit(FMLPreInitializationEvent event)
-    {
-//    	me.toast.leftclickcounter.keybinds.KeyBindings.init();
-//    	me.toast.leftclickcounter.keybinds.KeyBindings.init1();
-//    	me.toast.leftclickcounter.keybinds.KeyBindings.init11();
-//    	me.toast.leftclickcounter.keybinds.KeyBindings.init111();
-    }
+	
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
         ClientCommandHandler.instance.registerCommand(new LeftClickCounterCommand());
     	FMLCommonHandler.instance().bus().register(new LeftClickCounterListener());
-    	FMLCommonHandler.instance().bus().register(new RightClickCounterListener());
 
     }
 }
