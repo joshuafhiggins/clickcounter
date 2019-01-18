374aa3768acf485604aab7aa67fe2f2a7a9be898
Litl Toast <joshuafhiggins@gmail.com>
2019-01-18 17:51:11 -0500

    Partial 1.1.0

--- src/main/java/me/toast/leftclickcounter/listeners/LeftClickCounterListener.java
+++ src/main/java/me/toast/leftclickcounter/listeners/LeftClickCounterListener.java
@@ -1,6 +1,7 @@
 package me.toast.leftclickcounter.listeners;
 
-import me.toast.leftclickcounter.keybinds.KeyBindings;
+import me.toast.leftclickcounter.LeftClickCounterMod;
+import me.toast.leftclickcounter.settings.Settings;
 import me.toast.leftclickcounter.utils.RainbowUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
@@ -8,12 +9,19 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class LeftClickCounterListener 
 {
-
+	private LeftClickCounterMod mod;
+	
+	public LeftClickCounterListener(LeftClickCounterMod mod)
+	{
+		this.mod = mod;
+	}
+	
 	private int PVPClicks = 0;
 	private int PVPNumber1 = 0;
 	private int PVPNumber2 = 0;
@@ -21,8 +29,14 @@ public class LeftClickCounterListener
 	boolean isHeld = false;
 
 	private Minecraft mc = Minecraft.getMinecraft();
-
 	FontRenderer fr = mc.fontRendererObj;
+	
+	@SubscribeEvent
+	public void onClientTick(ClientTickEvent e)
+	{
+//		PVPNumber1 = (mod.getSettings().getposX());
+//		PVPNumber2 = (mod.getSettings().getposY());
+	}
 	
 	@SubscribeEvent
 	public void MouseInputEvent(InputEvent event)
@@ -32,6 +46,7 @@ public class LeftClickCounterListener
 			if (isHeld == false) 
 			{
 				PVPClicks++;
+//				mod.getSettings().setClicks(+1);
 				isHeld = true;
 			} 
 			else 
@@ -61,7 +76,7 @@ public class LeftClickCounterListener
 		for (int i = 0; i < "TestString".length(); i++) 
 		{
 			if (mc.theWorld != null) {
-				fr.drawStringWithShadow("PvP Clicks: " + PVPClicks, PVPNumber1, PVPNumber2,
+				fr.drawStringWithShadow("PvP Clicks: " + PVPClicks, mod.getSettings().getposX(), mod.getSettings().getposY(),
 						RainbowUtils.effect(i * 3500000L, brightness, 165).getRGB());
 			}
 		}
