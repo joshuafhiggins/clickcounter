20f0eb8a62bf0f68ad2940e4237b54c71728b39b
Litl Toast <joshuafhiggins@gmail.com>
2019-01-18 17:48:53 -0500

    Partial Update 1.1.0

--- src/main/java/me/toast/leftclickcounter/LeftClickCounterMod.java
+++ src/main/java/me/toast/leftclickcounter/LeftClickCounterMod.java
@@ -1,7 +1,11 @@
 package me.toast.leftclickcounter;
 
+import org.lwjgl.input.Keyboard;
+
 import me.toast.leftclickcounter.commands.LeftClickCounterCommand;
 import me.toast.leftclickcounter.listeners.LeftClickCounterListener;
+import me.toast.leftclickcounter.settings.Settings;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.common.MinecraftForge;
@@ -18,14 +22,21 @@ public class LeftClickCounterMod
     public static final String MODID = "leftclickcounter";
     public static final String VERSION = "1.1.0";
     
-	//TODO Fix en_US lang file for keybinds.
-	//TODO Make a config for storing clicks and positions for text.
-	
+    private Settings settings = new Settings();
+    
+	//TODO Fix command setting posX and posY.
+    //TODO Fix Clicks saving.
+   
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        ClientCommandHandler.instance.registerCommand(new LeftClickCounterCommand());
-    	FMLCommonHandler.instance().bus().register(new LeftClickCounterListener());
-
+        ClientCommandHandler.instance.registerCommand(new LeftClickCounterCommand(this));
+    	FMLCommonHandler.instance().bus().register(new LeftClickCounterListener(this));
+    	settings.loadConfig();
+    }
+    
+    public Settings getSettings() 
+    {
+    	return settings;
     }
 }
