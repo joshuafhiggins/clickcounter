96b078afe9e4d2be4f4ed3ac9bf72184262f7ed1
Litl Toast <joshuafhiggins@gmail.com>
2019-02-18 15:31:48 -0500

    Update 2.1.1

--- src/main/java/me/toast/leftclickcounter/LeftClickCounterMod.java
+++ src/main/java/me/toast/leftclickcounter/LeftClickCounterMod.java
@@ -1,29 +1,24 @@
 package me.toast.leftclickcounter;
 
-import org.lwjgl.input.Keyboard;
 import me.toast.leftclickcounter.commands.LcmPos;
 import me.toast.leftclickcounter.commands.LcmPrefix;
-import me.toast.leftclickcounter.commands.LcmPrefixReset;
+import me.toast.leftclickcounter.commands.LcmUpdate;
 import me.toast.leftclickcounter.listeners.LeftClickCounterListener;
+import me.toast.leftclickcounter.listeners.UpdateOnJoin;
 import me.toast.leftclickcounter.settings.Settings;
-import net.minecraft.client.settings.KeyBinding;
-import net.minecraft.init.Blocks;
 import net.minecraftforge.client.ClientCommandHandler;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = LeftClickCounterMod.MODID, version = LeftClickCounterMod.VERSION)
 public class LeftClickCounterMod
 {
     public static final String MODID = "leftclickcounter";
-    public static final String VERSION = "2.0.0";
+    public static final String VERSION = "2.1.1";
     
-    private Settings settings = new Settings();
+    private Settings Settings = new Settings();
     
     //TODO Fix the color customizbalty
     
@@ -32,13 +27,14 @@ public class LeftClickCounterMod
     {
         ClientCommandHandler.instance.registerCommand(new LcmPos(this));
         ClientCommandHandler.instance.registerCommand(new LcmPrefix(this));
-        ClientCommandHandler.instance.registerCommand(new LcmPrefixReset(this));
+        ClientCommandHandler.instance.registerCommand(new LcmUpdate());
     	FMLCommonHandler.instance().bus().register(new LeftClickCounterListener(this));
-    	settings.loadConfig();
+    	FMLCommonHandler.instance().bus().register(new UpdateOnJoin());
+    	Settings.loadConfig();
     }
     
     public Settings getSettings() 
     {
-    	return settings;
+    	return Settings;
     }
 }
